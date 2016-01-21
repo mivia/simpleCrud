443da71a263d0eee079ed58f6072a96e0f40a44f
mivia <wiaderekmichal@gmail.com>
2016-01-21 18:16:18 +0100

    recordStorage service added

---
+++ src/js/services/RecordStorage.js
@@ -0,0 +1,53 @@
+angular.module("test")
+.factory("RecordStorage", function($q){
+	this.storageID = 'records';
+	var store = this.store = {
+		records: [],
+
+		_saveToLS: function (records) {
+			localStorage.setItem(this.storageID, JSON.stringify(records));
+		},
+
+		_getFromLS: function () {
+			return JSON.parse(localStorage.getItem(this.storageID) || '[]');
+		},
+
+		get: function(){
+			var deferred = $q.defer();
+			angular.copy(store._getFromLS(), store.records);
+			deferred.resolve(this.records);
+
+			return deferred.promise;
+		},
+
+		insert: function(record){
+			var deferred = $q.defer();
+			this.records.push(record);
+			store._saveToLS(this.records);
+			deferred.resolve(this.records);
+
+			return deferred.promise;
+		},
+
+		delete: function(record){
+			var deferred = $q.defer();
+			this.records.splice(this.records.indexOf(record), 1);
+			store._saveToLS(this.records);
+			deferred.resolve(this.records);
+
+			return deferred.promise;
+		},
+
+		update: function(record, index){
+			var deferred = $q.defer();
+			this.records[index] = record;
+			store._saveToLS(this.records);
+			deferred.resolve(this.records);
+
+			return deferred.promise;
+		}
+	}
+
+	return store;
+});
+
